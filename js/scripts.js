a354a56ffa1d4d9395035966b84f29ab758ae3a3
Colin Worf and Sam Kissee <sam@>
2017-11-06 10:22:05 -0800

    add front end logic to show and hide adopted

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  $("form#animal").submit(function(event) {
+    event.preventDefault();
+    var animalType = $("select#pet-type").val();
+    var animalName = $("input#pet-name").val();
+    var animalAge = parseInt($("input#pet-age").val());
+    var animalSex = $("input[name=sex]:checked").val();
+    var animalBio = $('input#bio').val();
+    var newAnimal = new Animal (animalType, animalName, animalAge, animalSex, animalBio);
+  });
+  $('.adopt-btn').click(function(){
+    $(this).parent().addClass('adopted');
+  });
+  $('#show-all').click(function(){
+    $('.profile').show();
+  });
+  $('#show-avail').click(function(){
+    $('.profile').show();
+    $('.profile.adopted').hide();
+
+  });
+  $('#show-adopted').click(function(){
+    $('.profile').show();
+    $('.profile:not(.adopted)').hide();
+  });
+});
